1ad65b290b359b8a3b66476a39fdc42885ccad76
Dwan <dohwanz@naver.com>
2015-03-11 17:49:27 +0900

    Dwan's File

---
+++ f1.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+
+int main() {
+	return 0;
+}
+
+void f11(){
+	int a = 1;
+	printf("%d",a);
+}
+
+int f22() {
+	return 22;
+}
+
+int f33() {
+	int x = 10;
+	int y = 11;
+	int z;
+	z = x + y;
+	return z;
+}
